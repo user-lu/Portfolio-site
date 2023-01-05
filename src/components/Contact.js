cc068a68c256e8998e1bedf48971e178e8ed15c3
Luis Sevilla <user.sevilu6@gmail.com>
2023-01-05 16:51:32 -0600

    made small changes

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -66,6 +66,7 @@ export default function Contact() {
         <form
           name="contact"
           data-netlify="true"
+          method="post"
           onSubmit={handleSubmit}
           className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
           <input type="hidden" name="form-name" value="let's connect v1" />
