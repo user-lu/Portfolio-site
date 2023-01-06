c151b5a9a0b16011dc4d88bcbdffa26969976c80
Luis Sevilla <user.sevilu6@gmail.com>
2023-01-06 08:26:38 -0600

    fixed form reset issue

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -15,12 +15,17 @@ export default function Contact() {
 
   function handleSubmit(e) {
     e.preventDefault();
+
     fetch("/", {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
       body: encode({ "form-name": "contact", name, email, message }),
     })
-      .then(() => alert("Message sent!"))
+      .then(() => alert("Message sent!"), 
+      setName(''),
+      setEmail(''),
+      setMessage('')
+      )
       .catch((error) => alert(error));
   }
 
@@ -65,6 +70,7 @@ export default function Contact() {
         </div>
         <form
           name="contact"
+          action="/pages/success"
           netlify
           onSubmit={handleSubmit}
           className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
@@ -84,6 +90,7 @@ export default function Contact() {
               name="name"
               className="w-full bg-zinc-700 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
               onChange={(e) => setName(e.target.value)}
+              value={name}
             />
           </div>
           <div className="relative mb-4">
@@ -96,6 +103,7 @@ export default function Contact() {
               name="email"
               className="w-full bg-zinc-700 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
               onChange={(e) => setEmail(e.target.value)}
+              value={email}
             />
           </div>
           <div className="relative mb-4">
@@ -109,6 +117,7 @@ export default function Contact() {
               name="message"
               className="w-full bg-zinc-700 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
               onChange={(e) => setMessage(e.target.value)}
+              value={message}
             />
           </div>
           <button
